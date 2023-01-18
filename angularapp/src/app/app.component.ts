6c000d7a027f5267c6150cd9714bbd3ff65c6d5e
mathanNeoStark99 <mathankumar@iamneo.ai>
2023-01-18 11:26:48 +0000

    Get Node

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = "mathan's application";
+
+  constructor(public ser:UserService){}
+  ngOnInit(): void {
+    this.ser.getData().subscribe( res => {
+      console.log(res);
+    });
+  }
+
 }
